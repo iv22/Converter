2c5798a1b0c9d9490e43c38358db5f9761ac1b99
Ivan Ok <ask.ivankov@gmail.com>
2020-09-12 11:25:44 +0300

    i18n for custom validation

--- app/models/converter_validator.rb
+++ app/models/converter_validator.rb
@@ -1,7 +1,7 @@
 class ConverterValidator < ActiveModel::Validator
   def validate(record)
     if (record.amount.to_i) < 100
-      record.errors.add :base, "is too small"
+      record.errors.add :base, :is_too_small
     end
   end
 end
